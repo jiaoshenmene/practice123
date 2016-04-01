c15ceae2238fc5b9e5632983e932181e11bd78cf
杜甲 <815319775@qq.com>
2016-04-01 11:19:13 +0800

    commit

---
+++ practice123/BusinessJumpManager.m
@@ -0,0 +1,16 @@
+//
+//  BusinessJumpManager.m
+//  practice123
+//
+//  Created by 杜甲 on 16/4/1.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import "BusinessJumpManager.h"
+
+@implementation BusinessJumpManager
+- (void)jump
+{
+    
+}
+@end

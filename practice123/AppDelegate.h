6a173fd285696f27e50cbcee74e2b61afb4e68bd
杜甲 <815319775@qq.com>
2016-04-01 10:21:28 +0800

    commit

---
+++ practice123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practice123
+//
+//  Created by 杜甲 on 16/4/1.
+//  Copyright © 2016年 杜甲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
